b7197e3613e0cbbc6fb4408cd300eb2cae48731a
ArchUsr64 <anshulusr@gmail.com>
2023-08-12 16:42:53 +0530

    code refactor

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,6 @@ impl BinaryTrie {
             right: None,
         }
     }
-}
-
-impl BinaryTrie {
     fn insert(&mut self, value: &[bool]) {
         match value.split_first() {
             Some((first, rest)) => {
@@ -28,11 +25,7 @@ impl BinaryTrie {
                 match target_node {
                     &mut Some(ref mut child) => child.insert(rest),
                     &mut None => {
-                        let mut new_child = Self {
-                            filled: false,
-                            left: None,
-                            right: None,
-                        };
+                        let mut new_child = Self::new();
                         if take_left {
                             new_child.left = Some(Box::new(new_child.clone()));
                         } else {
@@ -49,20 +42,24 @@ impl BinaryTrie {
         }
     }
     fn search<'a>(&'a self, value: &'a [bool]) -> &[bool] {
-        let mut node = self;
-        let mut best_match = 0;
-        for (i, bit) in value.iter().enumerate() {
+        let mut node_to_search = self;
+        let mut best_match_index = 0;
+        for (depth, bit) in value.iter().enumerate() {
             let take_left = !*bit;
-            if node.filled {
-                best_match = i;
+            if node_to_search.filled {
+                best_match_index = depth;
             }
-            let target_node = if take_left { &node.left } else { &node.right };
+            let target_node = if take_left {
+                &node_to_search.left
+            } else {
+                &node_to_search.right
+            };
             match target_node {
-                Some(ref x) => node = x,
+                Some(ref x) => node_to_search = x,
                 None => break,
             }
         }
-        &value[..best_match]
+        &value[..best_match_index]
     }
 }
 
